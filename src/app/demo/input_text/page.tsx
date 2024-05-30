4c0768fbe9b0da0c8ce3179834e1760be0c7fd01
Nonthanisha <nonthanisha@gmail.com>
2024-05-30 20:24:56 +0700

    input text component

--- src/app/demo/input_text/page.tsx
+++ src/app/demo/input_text/page.tsx
@@ -3,16 +3,30 @@
 import React, { useState, useEffect } from 'react'
 import Inputtext from '@/components/inputtext/index'
 const page = () => {
-    const [inputText, setInputText] = useState<string>('')
-    const handleInput = (value:string) => {
-        console.log(value)
-        setInputText(value)
-    }
-    return (
-        <div>
-            <Inputtext value={inputText} label='input text' disabled={false} onChange={handleInput} placeholder="Please Enter inputtext"></Inputtext>
-        </div>
-    )
+    const [name, setName] = useState<string>('');
+  
+  // Example validation function: Allow only alphabets and spaces
+  const validateName = (value: string) => {
+    return /^[A-Za-z\s]+$/.test(value);
+  };
+
+  const handleNameChange = (newValue: string) => {
+    setName(newValue);
+  };
+
+  return (
+    <div>
+      <h1>Form</h1>
+      <Inputtext
+        label="Name"
+        value={name}
+        placeholder="Enter your name"
+        onChange={handleNameChange}
+        validate={validateName}
+        required // Adding required prop
+      />
+    </div>
+  );
 }
 
 export default page
